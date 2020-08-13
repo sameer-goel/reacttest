7c16344ca220594c401e6cb122aefb0fc7d0c15c
sameer-goel <goel.sa@husky.neu.edu>
2020-08-12 22:13:42 -0700

    Updates

---
+++ precache-manifest.04bc3c89494950bffb7e08a4a0ce3abd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968b99f6600e533928e7d6eaa83b6f4f",
+    "url": "/reacttest/index.html"
+  },
+  {
+    "revision": "cba9c52652a5d6acae54",
+    "url": "/reacttest/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "1eb74c18749c2017eba7",
+    "url": "/reacttest/static/css/main.33d55559.chunk.css"
+  },
+  {
+    "revision": "cba9c52652a5d6acae54",
+    "url": "/reacttest/static/js/2.5a88d3a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reacttest/static/js/2.5a88d3a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eb74c18749c2017eba7",
+    "url": "/reacttest/static/js/main.48986b62.chunk.js"
+  },
+  {
+    "revision": "ecf29c25536aa36208d2",
+    "url": "/reacttest/static/js/runtime-main.0a8c68ab.js"
+  }
+]);
